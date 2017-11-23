ffc9f39e0b1fa0a6fc04a44dd74eb5a19386f033
Max de D <m.dinechin@yahoo.com>
2017-11-23 22:57:09 +0100

    wrong image save location

--- attack.py
+++ attack.py
@@ -59,7 +59,7 @@ def compare(image1, r, image2, img_id, p, div):
     plt.axis('off')
     fig.tight_layout(pad=1)
     plt.subplots_adjust(left=0.05, right=0.95, top=0.80, bottom=0.05)
-    nom_image = "resultats/" + model_name + "_adv{}_n{}.png".format(img_id, p)
+    nom_image = "docs/images/attack_results" + model_name + "_adv{}_n{}.png".format(img_id, p)
     plt.savefig(nom_image, transparent=True)
     plt.show()
 
