9f15f34b9b14c65dc1b67def22363118efc47b9c
Max de D <m.dinechin@yahoo.com>
2017-12-19 20:49:14 +0100

    renamed CNN to AlexNet and removed useless architectures

--- architectures.py
+++ architectures.py
@@ -7,124 +7,9 @@ import torch
 from torch import nn
 
 
-class MLP(nn.Module):
+class AlexNet(nn.Module):
     def __init__(self):
-        super(MLP, self).__init__()
-        # Training hyperparameters
-        self.lr = 2e-4
-        self.epochs = 40
-        self.batch_size = 32
-        # Network definition
-        self.classifier = nn.Sequential(
-            nn.Linear(28 * 28, 120),
-            nn.ReLU(inplace=True),
-            nn.Linear(120, 120),
-            nn.ReLU(inplace=True),
-            nn.Linear(120, 10),
-            nn.Softmax()
-        )
-        # Optimizer and loss function
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        self.loss_fn = nn.CrossEntropyLoss()
-
-    def forward(self, x):
-        x = x.view(x.size(0), -1)  # Flatten  # Flatten
-        x = self.classifier(x)
-        return x
-
-
-class MLP_d(nn.Module):
-    def __init__(self):
-        super(MLP_d, self).__init__()
-        # Training hyperparameters
-        self.lr = 2e-4
-        self.epochs = 40
-        self.batch_size = 32
-        # Network definition
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(28 * 28, 120),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(120, 120),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(120, 10),
-            nn.Softmax()
-        )
-        # Optimizer and loss function
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        self.loss_fn = nn.CrossEntropyLoss()
-
-    def forward(self, x):
-        x = x.view(x.size(0), -1)  # Flatten  # Flatten
-        x = self.classifier(x)
-        return x
-
-
-class MLP_bn(nn.Module):
-    def __init__(self):
-        super(MLP_bn, self).__init__()
-        # Training hyperparameters
-        self.lr = 2e-4
-        self.epochs = 40
-        self.batch_size = 32
-        # Network definition
-        self.classifier = nn.Sequential(
-            nn.Linear(28 * 28, 120),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(120),
-            nn.Linear(120, 120),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm1d(120),
-            nn.Linear(120, 10),
-            nn.Softmax()
-        )
-        # Optimizer and loss function
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        self.loss_fn = nn.CrossEntropyLoss()
-
-    def forward(self, x):
-        x = x.view(x.size(0), -1)  # Flatten  # Flatten
-        x = self.classifier(x)
-        return x
-
-
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        # Training hyperparameters
-        self.lr = 2e-4
-        self.epochs = 40
-        self.batch_size = 32
-        self.features = nn.Sequential(
-            nn.Conv2d(1, 20, kernel_size=5),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(20, 40, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2)
-        )
-        self.classifier = nn.Sequential(
-            nn.Linear(40 * 5 * 5, 120),
-            nn.ReLU(inplace=True),
-            nn.Linear(120, 10),
-            nn.Softmax()
-        )
-        # Optimizer and loss function
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
-        self.loss_fn = nn.CrossEntropyLoss()
-
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)  # Flatten
-        x = self.classifier(x)
-        return x
-
-
-class CNN_d(nn.Module):
-    def __init__(self):
-        super(CNN_d, self).__init__()
+        super(AlexNet, self).__init__()
         # Training hyperparameters
         self.lr = 2e-4
         self.epochs = 40
@@ -156,9 +41,9 @@ class CNN_d(nn.Module):
         return x
 
 
-class CNN_bn(nn.Module):
+class AlexNet_bn(nn.Module):
     def __init__(self):
-        super(CNN_bn, self).__init__()
+        super(AlexNet_bn, self).__init__()
         # Training hyperparameters
         self.lr = 3e-5
         self.epochs = 40
